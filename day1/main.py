96dc98e6346a7fa883c876fb1e591cd3d136a297
Markus Rosenberg <markusrosenberg863@gmail.com>
2023-12-03 15:33:43 +0100

    ref

--- day1/main.py
+++ day1/main.py
@@ -1,5 +1,4 @@
-def part1():
-    lines = open("./input.txt", "r").readlines()
+def count_calibrations(lines: list[str]):
     result = 0
     
     for line in lines:
@@ -8,13 +7,18 @@ def part1():
         
         result += int(first_number) * 10 + int(second_number)
        
+    return result
+
+def part1():
+    lines = open("./input.txt", "r").readlines()
+    result = count_calibrations(lines)
+       
     print(result)
 
 def part2():
     lines = open("./input.txt", "r").readlines()
-    result = 0
     
-    for line in lines:
+    for (i, line) in enumerate(lines):
         line = (
             line.replace("one", "1")
                 .replace("two", "2")
@@ -25,14 +29,10 @@ def part2():
                 .replace("seven", "7")
                 .replace("eight", "8")
                 .replace("nine", "9"))
-        
-        first_number = next(c for c in line if c.isdigit())
-        second_number = next(c for c in reversed(line) if c.isdigit())
-        
-        result += int(first_number) * 10 + int(second_number)
+        lines[i] = line
        
+    result = count_calibrations(lines)
     print(result)
 
-       
 part1()
 part2()
